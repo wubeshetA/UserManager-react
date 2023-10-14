e2721a560dc91b55e391e2f23c5806ca56e04e6f
Wubeshet Yimam <w.yimam@alustudent.com>
2023-10-14 15:41:48 +0300

    Add NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./navbar/NavBar";
+
+const Home = () => <div>Home Page</div>;
+const Logs = () => <div>Logs Page</div>;
+
 function App() {
-
-
   return (
-    <h1 className="">
-      Hello world!
-    </h1>
-  )
+    <Router>
+      <Navbar />
+      <div className="p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/logs" element={<Logs />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/navbar/NavBar.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="bg-white p-4 border-b-2 shadow-lg">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-black text-2xl font-bold">LOGO</div>
+        
+        {/* Navbar items for large screens */}
+        <div className="hidden md:flex space-x-4">
+          <Link to="/" className="text-black">Home</Link>
+          <Link to="/logs" className="text-black">Logs</Link>
+        </div>
+        
+        {/* Hamburger menu for small screens */}
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-black">
+            <GiHamburgerMenu size={24} />
+          </button>
+        </div>
+        
+        {/* Mobile menu */}
+        {isOpen && (
+          <div className="md:hidden absolute top-16 right-0 bg-white w-3/4 py-2 border-b-2 shadow-lg transition duration-500 ease-in-out transform translate-x-0"> {/* Add width and transition */}
+            <Link to="/" className="block text-black py-2 px-4 border-b">Home</Link>
+            <Link to="/logs" className="block text-black py-2 px-4 border-b">Logs</Link>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
